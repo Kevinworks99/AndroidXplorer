df3696e518e953441c1372597ca196cc48b46396
Kevin <kevin.dhameliya@iflair.com>
2025-05-12 18:00:17 +0530

    Remove log statement

--- app/src/main/java/com/adroid/androidxplorer/components/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/adroid/androidxplorer/components/fragments/home/HomeFragment.kt
@@ -1,7 +1,6 @@
 package com.adroid.androidxplorer.components.fragments.home
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -35,7 +34,6 @@ class HomeFragment : Fragment(), View.OnClickListener {
 
         setupCategoryRecycler()
         setupProductRecycler()
-        Log.d("TAG", "onViewCreated: New branch check")
     }
 
     /**
